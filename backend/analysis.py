79ea95d407b0f2e88b01343f2b4b603e180021f3
Kenneth Lee <kennethslee.613@gmail.com>
2019-11-27 23:52:33 -0500

    analyzing winrates for each champion

--- backend/analysis.py
+++ backend/analysis.py
@@ -1,17 +1,6 @@
 import pymysql.cursors
 import json
 
-DB_INFO = ''
-with open('./DATA_CONSTRUCTION/DB_INFO.txt', 'r') as key:
-    DB_INFO = [line.rstrip('\n') for line in key]
-
-connection = pymysql.connect(host=DB_INFO[0],
-                             port=int(DB_INFO[1]),
-                             user=DB_INFO[2],
-                             password=DB_INFO[3],
-                             db=DB_INFO[4],
-                             charset='utf8mb4')
-
 def getChampionIdDict():
     champions = {}
     with open('championData.json') as json_file:
@@ -20,7 +9,7 @@ def getChampionIdDict():
             champions[champ['key']] = champ['name']
     return champions
 
-def goThroughTeams(championIds):
+def goThroughTeams(championIds, connection):
     cursor = connection.cursor()
     for champId in championIds:
         print(champId, end='\r')
@@ -42,16 +31,50 @@ def goThroughTeams(championIds):
                 updateQuery = 'UPDATE GameData SET checked = 1 WHERE checked = 0 AND gameId = %s;'
                 cursor.execute(updateQuery, game[0])
 
+def findBestPairs(champId, championIds, championIdsSorted, connection):
+    cursor = connection.cursor()
+    query = 'SELECT * FROM ChampionData WHERE championId = %s'
+    cursor.execute(query, (champId))
+    response = cursor.fetchall()[0][1:]
+    winrates = []
+    for i in range(0, len(response), 2):
+        if response[i] + response[i + 1] == 0:
+            winrates.append(None)
+        elif response[i] + response[i + 1] < 10 or response[i]/(response[i] + response[i + 1]) < 0.5:
+            winrates.append(None)
+        else:
+            winrates.append((response[i]/(response[i] + response[i + 1]), response[i] + response[i + 1], championIds[str(championIdsSorted[i // 2])]))
+    print(winrates)
+    for x in winrates:
+        if x != None:
+            print(x[2])
+
 if __name__ == '__main__':
+    DB_INFO = ''
+    with open('./DATA_CONSTRUCTION/DB_INFO.txt', 'r') as key:
+        DB_INFO = [line.rstrip('\n') for line in key]
+
+    connection = pymysql.connect(host=DB_INFO[0],
+                             port=int(DB_INFO[1]),
+                             user=DB_INFO[2],
+                             password=DB_INFO[3],
+                             db=DB_INFO[4],
+                             charset='utf8mb4')
+
     championIds = getChampionIdDict()
     championIdsSorted = []
     for x in championIds.keys():
         championIdsSorted.append(int(x))
     championIdsSorted.sort()
     try:
-        goThroughTeams(championIdsSorted)
-        
+        goThroughTeams(championIdsSorted, connection)
         connection.commit()
+        print('Finished updating pairs.')
     except:
         connection.rollback()
+        print('Updating pairs has failed.')
+
+    findBestPairs('421', championIds, championIdsSorted, connection)
+    
     connection.close()
+
