a34f469b3c0ee3ffa5432c5fe163e74b2551b05a
Philip Jung <philip.jung.98@gmail.com>
2019-12-02 11:24:42 -0500

    added basic routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import ChampionTable from './components/ChampionTable.js'
 
 function App() {
   return (
     <div className="App">
-      <ChampionTable />
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={ChampionTable} />
+          {/* <Route exact path="/champion/:id" component={WIP} /> */}
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
