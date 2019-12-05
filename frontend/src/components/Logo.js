464748e67540f8b26cdc9daba63f10854cbcbada
josephjdkim <josephjdkim@gmail.com>
2019-12-05 18:36:48 -0500

    fixed underlining and changed styling

--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -6,7 +6,7 @@ class Logo extends Component {
   render() {
     return (
       <Fragment>
-        <Link to={ '/' }>
+        <Link to={ '/' } style={{textDecoration: 'none'}}>
           <div style={{marginBottom: '15px'}}>
             <img src="logo.png" style={{width: '100px'}}/>
             <h1 className="header">League of Data</h1>
