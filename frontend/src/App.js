926f5b9e9bbb623b6199e9397f1ba9c3cd4849e9
Philip Jung <philip.jung.98@gmail.com>
2019-12-11 02:08:22 -0500

    changed to HashRouter for github pages compatibility

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home'
 import ChampionPage from './components/ChampionPage.js'
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <Switch>
           <Route exact path="/" component={ Home } />
-          <Route exact path="/pairs" component={ ChampionPage } />
+          <Route path="/pairs" component={ ChampionPage } />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
