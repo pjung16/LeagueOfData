ee60b91acff8f82709607728e298dd4658af5dbd
josephjdkim <josephjdkim@gmail.com>
2019-12-12 15:10:37 -0500

    about page

--- frontend/src/components/AboutPage.js
+++ frontend/src/components/AboutPage.js
@@ -35,14 +35,17 @@ class BestTeamsPage extends Component {
     return (
       <div className="container" style={{maxWidth: '950px', color: '#ffffff'}}>
         <div className="about-header">
-          <h1>About Us</h1>
+          <h1>About</h1>
         </div>
         <p>
-          blablablablablababl
+          League of Data is data-driven web application that returns ideal champion pairings for the game League of Legends. The application uses Riot's very own API to collect match data from Challenger players.
+        </p>
+        <p>
+          For a more technical and detailed understanding of the application, <a className="about-link" href="https://github.com/pjung16/LeagueOfData" target="blank">visit its GitHub repository.</a>
         </p>
         <br />
         <p>
-          blablablablablababl
+          League of Data was developed by <a className="about-link" href="https://www.kennethslee.com/" target="blank">Kenneth Lee</a>, <a className="about-link" href="https://pjung16.github.io/" target="blank">Philip Jung</a>, and <a className="about-link" href="https://www.josephjd.kim/" target="blank">Joseph Kim</a>.
         </p>
       </div>
     )
